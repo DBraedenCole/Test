88c4d9dc79bcd068d36fa6a4f4114febb7760a5b
Braeden Cole <braeden_cole1@baylor.edu>
2023-01-31 15:16:46 -0600

    creat py file for testing

---
+++ test.py
@@ -0,0 +1,10 @@
+def main():
+    myDict = {}
+    myDict = {"test1": 1, "test2": 2, "test3": 3}
+    print(myDict)
+
+    for k, v in myDict.items():
+        print(f"Key: {k} AND Value: {v}")
+
+
+main()
